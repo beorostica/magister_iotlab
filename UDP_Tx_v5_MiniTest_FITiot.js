72de51d9d1b4402cb84844dcffb5a56810728377
beorostica <36004160+beorostica@users.noreply.github.com>
2018-04-09 18:11:10 -0300

    Add files via upload

---
+++ UDP_Tx_v5_MiniTest_FITiot.js
@@ -0,0 +1,40 @@
+
+
+var udp_datagram  = require('dgram');
+var IP_broadcast = '10.0.1.255';
+var client       = udp_datagram.createSocket('udp4');
+
+
+//****************************************************************************//
+//*** Send UDP datagram to Broadcast *****************************************//
+//****************************************************************************//
+
+//Let broadcast transmission:
+client.bind( 
+    function(){ 
+        client.setBroadcast(true) 
+    } 
+);
+
+//Each step, send UDP datagram:
+function send_UDPdatagram(){
+    
+    var message = new Buffer("start");
+    
+    client.send(
+        message, 0, message.length, 4000, IP_broadcast, 
+        function(err,bytes){
+            if(err){
+                throw err;    
+            }
+        }
+    );
+    
+    //Console print for debugging:
+    console.log('UDP message sent: ' + message);
+
+}
+
+setInterval(send_UDPdatagram, 2000);
+
+//****************************************************************************//
