0b652adf0b12e463b961f58b349c90108caef6d3
beorostica <36004160+beorostica@users.noreply.github.com>
2018-04-16 16:04:44 -0300

    Update IPv6_6agents_2.js

--- IPv6_6agents_2.js
+++ IPv6_6agents_2.js
@@ -4,7 +4,7 @@
 /////////////////////////////////////////////////////////////
 
 var myIPaddress = '2001:660:4701:f080::2';
-var neighbor    = ['2001:660:3207:0400::8c', '2001:660:5307:3000::14'];
+var neighbor    = ['2001:660:5307:3000::6b', '2001:660:5307:3000::67'];
 var initial_value  = 90;        //Initial Measurement
 var timer_interval = 1000;      //Timer frecuency
 
