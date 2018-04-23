036478e65e76e8339c8738b3fb3db1627cfa7091
beorostica <36004160+beorostica@users.noreply.github.com>
2018-04-23 17:10:35 -0300

    Delete commands.sh

--- commands.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-iotlab-ssh --verbose run-cmd "cd A8 && node IoTLab_protocol.js" -l grenoble,a8,102+100+95 &
-iotlab-ssh --verbose run-cmd "cd A8 && node IoTLab_protocol.js" -l saclay,a8,100+103+102 &
-iotlab-ssh --verbose run-cmd "cd A8 && node IoTLab_protocol.js" -l strasbourg,a8,13+10+11+12 &
-sleep 30
-iotlab-ssh --verbose run-cmd "cd A8 && node IoTLab_start_signal.js" --frontend
-sleep 2m
-iotlab-ssh --verbose run-cmd "cd A8 && node IoTLab_stop_signal.js" --frontend
